2e6a8d9584501460e0473fc58f1f4f35ad2d5996
guxiken9 <guxiken9@gmail.com>
2023-12-06 14:58:58 +0900

    全部出るように戻し

--- src/app/lib/component/PhotoList.tsx
+++ src/app/lib/component/PhotoList.tsx
@@ -5,35 +5,17 @@ import { Post } from "../type"
 
 export const PostsList: FunctionComponent<{ posts: Post[] }> = ({ posts }) => {
     return (
-        <div className="grid grid-col-3 gap-4 w-[1200px] mx-auto">
-            {
-                [0, 1, 2].map((columnIndex) => (
-                    <div key={columnIndex}>
-                        {
-                            posts.map((post, index) => {
-                                if (columnIndex % 3 === index) {
-                                    return (
-                                        <div
-                                            key={post.id}
-                                            className="mb-4 last:mb-0"
-                                        >
-                                            <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                                                <div className="px-6 py-4">
-                                                    <div className="font-bold text-xl mb-2">{post.title}</div>
-                                                    <p className="text-base">
-                                                        {post.content}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    );
-                                }
-                                return null;
-                            })
-                        }
+        <div className="grid grid-cols-3 gap-4 w-[1200px] mx-auto">
+            {posts.map((post, index) => (
+                <div key={index} className="max-w-sm rounded overflow-hidden shadow-lg">
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">{post.title}</div>
+                        <p className="text-base">
+                            {post.content}
+                        </p>
                     </div>
-                ))
-            }
+                </div>
+            ))}
         </div>
-    );
+    )
 };
