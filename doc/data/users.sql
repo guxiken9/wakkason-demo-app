3b5f580bd123eb9accf6797b72134d9a235b34ba
annkara <howtobecomeahacker@gmail.com>
2023-12-11 14:08:18 +0900

    DDLとテストデータを追加

---
+++ doc/data/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, email, password_hash)
+VALUES
+    ('test_user1', 'test_user1@example.com', 'hashed_password1'),
+    ('test_user2', 'test_user2@example.com', 'hashed_password2');
