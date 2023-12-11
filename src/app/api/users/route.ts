4e45a484b21114357acf912171e5c77dd0b86b73
annkara <howtobecomeahacker@gmail.com>
2023-12-11 17:15:25 +0900

    シンプル化

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,18 @@
-import { getUsers } from "@/app/lib/users";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
 
 export async function GET(request: Request) {
     const result = await getUsers();
     return new Response(JSON.stringify(result), {
         status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        }
     });
+}
+
+export const getUsers: any = async () => {
+    const users = prisma.users.findMany()
+    return users;
 }
--- src/app/lib/users.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient()
-export const getUsers: any = async () => {
-    const users = prisma.users.findMany()
-    return NextResponse.json(users);;
-}
