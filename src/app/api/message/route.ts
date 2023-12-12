23a5d24e7130193b02529006457cdbda0ed4c6dc
annkara <howtobecomeahacker@gmail.com>
2023-12-13 07:24:24 +0900

    デバッグ出力

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -5,17 +5,22 @@ const prisma = new PrismaClient()
 
 export async function POST(request: Request) {
     const message: Message = await request.json();
-    const result = await postMessage(message);
+    console.log("---------------")
+    console.log(message)
+    console.log("---------------")
+
+    const result = prisma.messages.create({
+        data: {
+            recipient_id: message.recipient_id,
+            title: message.title,
+            scheduled_time: message.scheduled_time,
+            message: message.message,
+        }
+    })
+    console.log(result);
     
     return new Response(JSON.stringify(result), {
         status: 200,
     });
     
-}
-
-const postMessage = async (m: Message) => {
-    const result = prisma.messages.create({
-        data:m
-    })
-    return result;
 }
