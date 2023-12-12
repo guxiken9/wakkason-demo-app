7a23c9e7ecac479f32429529bbc049c110492867
annkara <howtobecomeahacker@gmail.com>
2023-12-13 06:29:51 +0900

    コンポーネントをページ直接記述

--- src/app/lib/component/PostMessage.tsx
+++ src/app/lib/component/PostMessage.tsx
@@ -1,10 +1,27 @@
-import { Input } from "./atoms/Input";
-import { Label } from "./atoms/Label";
-import { SubmitButtom } from "./atoms/SubmitButton";
-import { TextArea } from "./atoms/TextArea";
-import { Select } from "./atoms/select";
+"use client";
+
+import { useEffect, useState } from "react";
+import { Users } from "../type";
 
 export const PostMessage = () => {
+  const [users, setUsers] = useState<Users>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`api/users`, {
+          method: "GET",
+        });
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
       <div className="mx-auto max-w-2xl">
@@ -14,25 +31,71 @@ export const PostMessage = () => {
         <div className="mt-5 p-4 relative z-10 bg-white border rounded-xl sm:mt-10 md:p-10">
           <form>
             <div className="mb-4 sm:mb-8">
-              <Label text="To"></Label>
-              <Select></Select>
+              <label
+                htmlFor="hs-feedback-post-comment-textarea-1"
+                className="block mb-2 text-sm font-medium"
+              >
+                To
+              </label>
+              <select
+                id="countries"
+                className=" border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                {users.map((user) => (
+                  <option key={user.user_id} value={user.user_id}>
+                    {user.username}
+                  </option>
+                ))}
+              </select>
             </div>
             <div className="mb-4 sm:mb-8">
-              <Label text="Date"></Label>
-              <Input placeholder="9999-12-31"></Input>
+              <label
+                htmlFor="hs-feedback-post-comment-textarea-1"
+                className="block mb-2 text-sm font-medium"
+              >
+                Date
+              </label>
+              <input
+                type="text"
+                className="py-3 px-4 block w-full border-gray-400 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-100 dark:text-gray-900 dark:focus:ring-gray-600"
+                placeholder="9999-12-31"
+              />
             </div>
             <div className="mb-4 sm:mb-8">
-              <Label text="Title"></Label>
-              <Input placeholder="Title"></Input>
+              <label
+                htmlFor="hs-feedback-post-comment-textarea-1"
+                className="block mb-2 text-sm font-medium"
+              >
+                Title
+              </label>
+              <input
+                type="text"
+                className="py-3 px-4 block w-full border-gray-400 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-100 dark:text-gray-900 dark:focus:ring-gray-600"
+                placeholder="Title"
+              />
             </div>
             <div>
-              <Label text="Message"></Label>
+              <label
+                htmlFor="hs-feedback-post-comment-textarea-1"
+                className="block mb-2 text-sm font-medium"
+              >
+                Message
+              </label>
               <div className="mt-1">
-                <TextArea placeholder="Leave a message here ..."></TextArea>
+                <textarea
+                  name="hs-feedback-post-comment-textarea-1"
+                  className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-100 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                  placeholder="Leave a message here ..."
+                ></textarea>
               </div>
             </div>
             <div className="mt-6 grid">
-              <SubmitButtom name="Submit"></SubmitButtom>
+              <button
+                type="submit"
+                className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              >
+                Submit
+              </button>
             </div>
           </form>
         </div>
